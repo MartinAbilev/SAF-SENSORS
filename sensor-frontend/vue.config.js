36c17492e981119de99e5a81c90a8de4bf4e56cb
Martin <martin.abilev@gmail.com>
2024-10-02 12:08:03 +0300

    forgot its prod in docker

--- sensor-frontend/vue.config.js
+++ sensor-frontend/vue.config.js
@@ -3,7 +3,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer:
   {
-    port: process.env.VUE_APP_PORT || 8080, // Use the port from the environment variable or fall back to 8080
+    port: process.env.VUE_APP_PORT || 3333, // Use the port from the environment variable or fall back to 3333
   }
 
 })
